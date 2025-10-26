a1234d5c6dcfa3854a4d44be02b458baa69f5a98
schokostulle <schokostulle.777@gmail.com>
2025-10-26 22:48:29 +0100

    Aktualisieren von supabase.js

--- js/supabase.js
+++ js/supabase.js
@@ -13,8 +13,8 @@ import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js
 // 2. "anon public key"
 // ---------------------------------------------------------
 
-const SUPABASE_URL = 'https://DEINPROJECT.supabase.co';      // ← ersetzen
-const SUPABASE_ANON_KEY = 'DEIN_PUBLIC_ANON_KEY';            // ← ersetzen
+const SUPABASE_URL = 'https://xgdybrinpypeppdswheb.supabase.co';      // ← ersetzen
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhnZHlicmlucHlwZXBwZHN3aGViIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjA5ODEwOTUsImV4cCI6MjA3NjU1NzA5NX0.cphqzda66AqJEXzZ0c49PZFM8bZ_eJwjHaiyvIP_sPo';            // ← ersetzen
 
 // ---------------------------------------------------------
 // Supabase Client erstellen
